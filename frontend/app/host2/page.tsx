a676e655966af44e630be34b4482201373a99615
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-02-20 00:46:22 +0100

    + Host Page updated => Switch from Template Based to dynamical creation of game Rooms, where persons DONT get hardcoded, but get added to the game as new clients join and enter a name

--- frontend/app/host2/page.tsx
+++ frontend/app/host2/page.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { useRef, useState } from "react"
 import { Mode } from "../lib/types"
 import { Modes } from "../lib/types"
+import { select } from "framer-motion/client"
 
 interface RoomConfig {
     roomName: string | undefined,
@@ -17,27 +18,29 @@ interface RoomConfig {
 
 export default function Host() {
     const [roomConfig, setRoomConfig] = useState<RoomConfig | null>(null)
-    const [selectedMode, setSelectedMode] = useState<Mode>(Modes[0]);
+    const [selectedMode, setSelectedMode] = useState<Mode>("wheel");
     const roomNameRef = useRef<HTMLInputElement>(null)
     const privateRoomRef = useRef<HTMLInputElement>(null)
     const router = useRouter()
 
-    function updateRoomConfig() {
-        setRoomConfig({ roomName: roomNameRef.current?.value, privateRoom: privateRoomRef.current?.checked })
-
-        console.log(roomNameRef.current?.value)
-        console.log(privateRoomRef.current?.checked)
-    }
 
     async function startGame() {
         try {
+            const currentRoomConfig = {
+                roomName: roomNameRef.current?.value,
+                privateRoom: privateRoomRef.current?.checked,
+                gameMode: selectedMode
+            }
+
+            console.log("Starting with config: ", currentRoomConfig)
+
             const res = await apiFetch("/api/game/start", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 redirectAuth: false,
-                body: JSON.stringify({ roomConfig })
+                body: JSON.stringify({ currentRoomConfig })
             })
             if (res.success) {
                 console.log("New Game started successfully. Rederecting now.")
@@ -65,17 +68,22 @@ export default function Host() {
             </div >
             <div className="flex flex-row gap-5 w-fit">
                 <div className="flex flex-col gap-3 justify-center w-30">
-                    <Input placeholder="Room Name" width="w-full" id="roomName" ref={roomNameRef} onChange={updateRoomConfig} />
+                    <Input placeholder="Room Name" width="w-full" id="roomName" ref={roomNameRef} />
                     <div className="flex flex-row h-fit w-full border-2 p-2 border-black/20 rounded-2xl">
                         <label className="w-full mr-2 select-none" htmlFor="is-private">Private</label>
-                        <Input type="checkbox" width="w-5" height="h-4" className="self-center" id="is-private" ref={privateRoomRef} onChange={updateRoomConfig} />
+                        <Input type="checkbox" width="w-5" height="h-4" className="self-center" id="is-private" ref={privateRoomRef}/>
                     </div>
                 </div>
                 <div className="flex flex-col gap-3 h-full w-30 border-2 p-2 border-black/20 rounded-2xl">
                     <label className="w-25 select-none justify-items-center items-center" htmlFor="is-private">Game mode</label>
-                    <select id="modeSelect" className="border-none border-black/20 rounded-2xl p-0 w-full h-full">
+                    <select
+                        id="modeSelect"
+                        onChange={((e: React.ChangeEvent<HTMLSelectElement>)=> setSelectedMode(e.target.value as Mode))}
+                        value={selectedMode}
+                        className="border-none border-black/20 rounded-2xl p-0 w-full h-full"
+                    >
                         {Modes && Modes.map((m) => {
-                            return(
+                            return (
                                 <option key={m} className="bg-black/5 border-2 border-black rounded-2xl" value={m}>{m}</option>
                             )
                         })}
