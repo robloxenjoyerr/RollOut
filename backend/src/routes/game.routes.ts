d72613a6cb03aaefda753658e33874e2b7e7fb24
NikolaLaptop <NikolaLaptop>
2026-02-20 14:14:56 +0100

    + .

--- backend/src/routes/game.routes.ts
+++ backend/src/routes/game.routes.ts
@@ -1,60 +1,33 @@
-import { db } from "../db/database";
 import { Router } from "express";
-import { idFromToken } from "../lib/services";
-import { loginAuthentication } from "../middleware/secureMiddleware";
-import { startGame, checkUserForActiveSession, checkIfGameIdExist, stopGame, getLiveGameById, getTemplateFromGameId } from "../lib/game-manager";
+import { stopGame } from "../lib/game-manager";
 
 
 const gameRouter = Router()
 
 
-gameRouter.get("/from-session/:session_id", loginAuthentication, async (req, res) => {
-    const { session_id } = req.params;
-    const game = db.prepare(`SELECT id FROM live_games WHERE session_id = ? AND ended_at IS NULL`).get(session_id) as { id: string } | undefined
 
-    if (!game) return res.status(404).send({ success: false, message: "Session not found." });
-
-    return res.send({ success: true, game_id: game.id });
-});
 
 gameRouter.post("/verify", async (req, res) => {
     const { game_id_url } = req.body
     const authHeader = req.headers.authorization
     const token = authHeader?.split(" ")[1]
-    const user_id = idFromToken(token)
+
     try {
-        const info = await checkIfGameIdExist(game_id_url)
-        if (!info) return res.send({ success: false })
 
-        const { id, host_id } = info
-        const game = await getLiveGameById(id)
-        const game_template = await getTemplateFromGameId(id)
-        if (!game) return res.send({ success: false, message: "Couldnt get a valid GamePhase from DB function." })
-
-        if (id) {
-            if (user_id === host_id) return res.send({ success: true, host: true, game_phase: game.phase, game_template: game_template })
-            else return res.send({ success: true, host: false, game_phase: game.phase, game_template: game_template })
-        }
-        else return res.send({ success: false })
     } catch (err) {
-        console.log("gameRouter /verify ERROR: ", err)
+
     }
 })
 
 gameRouter.post("/start", async (req, res) => {
     try {
-        const info = await startGame(template, owner_id)
-        if (info.success) {
-            console.log("Game with gameID: ", info.game_id, "started.")
-            return res.send({ success: true, game_id: info.game_id, session_id: info.session_id, host: true })
-        }
+
     } catch (err) {
-        console.log(err)
-        return res.status(500).send({ success: false, message: "Server error." })
+
     }
 })
 
-gameRouter.post("/stop", loginAuthentication, async (req, res) => {
+gameRouter.post("/stop", async (req, res) => {
     const { game_id } = req.body
     try {
         const info = await stopGame(game_id)
