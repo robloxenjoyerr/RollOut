67547cc190edea99c066413441ce072d1fbd2c7b
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-02-25 00:01:56 +0100

    ~ .

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express"
-import { getLiveGames } from "../lib/game-manager.js"
 import gameRouter from "./game.routes.js"
 import userRouter from "./user.routes.js"
 import templateRouter from "./templates.routes.js"
@@ -12,9 +11,7 @@ apiRouter.use("/templates", templateRouter)
 apiRouter.use("/game", gameRouter)
 
 apiRouter.get("/livegames", async (req, res) => {
-  const liveGames = await getLiveGames()
-
-  return res.send({ liveGames })
+  
 })
 
 export default apiRouter
