95208800ece8bf417229d5283f6a8a02814e0495
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-02-25 00:31:01 +0100

    ~added driveradapter to prismaclient => version 7 needs it

--- backend/src/lib/prisma-client.ts
+++ backend/src/lib/prisma-client.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-const prisma = globalForPrisma.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!
+})
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
 export default prisma
