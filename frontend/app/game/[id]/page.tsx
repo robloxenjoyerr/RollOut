b432bbc0dce65d1b87be5d9171136c07a0deee7e
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-02-25 23:25:47 +0100

    ~ added error handling on /game/id page to find bug source why only blank screen is showing

--- frontend/app/game/[id]/page.tsx
+++ frontend/app/game/[id]/page.tsx
@@ -3,15 +3,19 @@ import { apiFetch } from "@/app/lib/api"
 import { redirect } from "next/navigation"
 import GameClientView from "@/app/components/GameClientView"
 import GameHostView from "@/app/components/GameHostView"
+import { cookies } from "next/headers"
 
 
 export default async function Page({ params }: { params: { roomId: string } }) {
     const { roomId } = await params;
+    const cookieStore = await cookies()
+    const hostId = cookieStore.get("hostId")?.value
 
     try {
         const res = await apiFetch(`/api/game/verify/${roomId}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
+            ...(hostId ? { "Cookie: ": `hostId=${hostId}`} : {}),
             cache: "no-store",
             redirectAuth: false // so apiFetch doesnt redirect by itself
         });
@@ -26,6 +30,7 @@ export default async function Page({ params }: { params: { roomId: string } }) {
     } catch (error) {
         // Tun als ob Antwort erfolgreich, aber "host" ist false.
         console.error("game/id ERROR : ", error)
+        return redirect("/")
     }
 
 
