5c62149069bc4f8a397376e247e6473f5cdfc43a
NikolaLaptop <NikolaLaptop>
2026-02-26 09:23:38 +0100

    ~

--- frontend/app/components/GameClientView.tsx
+++ frontend/app/components/GameClientView.tsx
@@ -161,7 +161,7 @@ export default function GameClientView({ game_id, game_phase }: GameClientViewPr
         // The dependency array should only include values that when changed require the effect to be re-run.
     }, [socket, game_id, addToast, router, game_phase])
 
-    if (currentPhase === "waiting") {
+    if (currentPhase === "waiting-lobby") {
         return (
             <div>
                 <AnimatePresence>
