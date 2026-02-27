54fe3ab8d659c512da8418c049af539e7efcb332
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-02-27 02:03:53 +0100

    ~ Deleted unused api.routes => template, user

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express"
 import gameRouter from "./game.routes"
 import userRouter from "./user.routes"
-import templateRouter from "./templates.routes"
 
 const apiRouter = Router()
 
 
 apiRouter.use("/user", userRouter)
-apiRouter.use("/templates", templateRouter)
 apiRouter.use("/game", gameRouter)
 
 apiRouter.get("/livegames", async (req, res) => {
