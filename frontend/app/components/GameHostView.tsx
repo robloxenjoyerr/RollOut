df876179e22b0f0467791c68e69ca122269bf8c7
NikkiSRB <96006252+NikkiSRB@users.noreply.github.com>
2026-03-04 23:43:36 +0100

    ~ Added currentPhase in dependency array in host view

--- frontend/app/components/GameHostView.tsx
+++ frontend/app/components/GameHostView.tsx
@@ -166,7 +166,7 @@ export default function GameHostView({ roomCode, clientId, roomConfig }: GameHos
 
 
         //  runs when the component unmounts or dependencies change
-        // removing ALL listeners to prevent memory leaks
+        // removing ALL listeners to prevent memory 
         return () => {
             console.log("Cleaning up socket listeners...")
             socket.off("connect", onConnect)
@@ -180,7 +180,7 @@ export default function GameHostView({ roomCode, clientId, roomConfig }: GameHos
             socket.off("allPersonsRolled", onAllRolled)
         }
         // The dependency array should only include values that when changed require the effect to be re-run.
-    }, [socket, roomCode])
+    }, [socket, roomCode, currentPhase])
 
 
     const rollNext = () => {
